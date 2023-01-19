3d93790279ab5c39492a241dfbaa244ef7e1680c
Krishna <krishnabiswakarmasc@gmail.com>
2023-01-19 12:05:39 +0530

    Temporary commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import styles from './App.module.css';
 import Card from './Components/Card/Card'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar/Navbar';
+import { useState, useEffect } from 'react';
 const App = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => {
+        const categorySet = new Set();
+        data.forEach(item => categorySet.add(item.category));
+        setCategories([...categorySet]);
+      })
+  }, [])
   return (
     <div className={`${styles.app__wrapper} container`}>
-      <Navbar />
+      <Navbar categories={categories} />
       <div className={styles.app}>
         <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+
+
       </div>
     </div>
   )
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,19 @@
 import styles from './Navbar.module.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-const Navbar = () => {
+import { useState } from 'react';
+const Navbar = ({ categories }) => {
     return (
         <nav className={styles.nav}>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success" type="submit">Search</button>
+            <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
             <div className="dropdown">
                 <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</button>
                 <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    {categories.map((category, index) => {
+                        return <li key={index}><a className="dropdown-item" href="#">{category}</a></li>
+                    })}
                 </ul>
             </div>
         </nav>
